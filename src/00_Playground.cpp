ebb5f7987af74fc704162fbf035a990869549d6e
Stephen NG <15920902266@139.com>
2024-11-23 17:57:26 +0800

    Init setup

---
+++ src/00_Playground.cpp
@@ -0,0 +1,21 @@
+// === Polyscope ===
+#include "polyscope/polyscope.h"
+
+// === Geometry-Central ===
+#include "geometrycentral/surface/manifold_surface_mesh.h"
+#include "geometrycentral/surface/vertex_position_geometry.h"
+#include "geometrycentral/surface/meshio.h"
+
+// === Eigen ===
+#include <Eigen/Core>
+
+// === libigl ===
+#include <igl/writeOBJ.h>
+
+// === Spectra ===
+#include <Spectra/SymGEigsShiftSolver.h>
+
+
+int main(int argc, char** argv) {
+	std::cout << "Hello!" << std::endl;
+}
